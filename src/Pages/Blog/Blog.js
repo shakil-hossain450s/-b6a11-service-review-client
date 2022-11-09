808325a572a193b5258a78561306b8f5707547cf
Shakil <shakil.hossain450s@gmail.com>
2022-11-09 09:28:55 +0600

    blog section updated

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { FaArrowAltCircleRight } from "react-icons/fa";
+import BlogCard from './BlogCard';
 
 const Blog = () => {
     const [blogQuestions, setBlogQuestions] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:5000/blog')
+        fetch('blog.json')
             .then(res => res.json())
             .then(data => setBlogQuestions(data))
     }, [])
@@ -13,20 +13,10 @@ const Blog = () => {
             <h2 className='text-4xl text-center my-5 font-semibold'>Here is your some question</h2>
             <div>
                 {
-                    blogQuestions.map(blogQuestion => <li
-                        className='text-3xl my-16 '
+                    blogQuestions.map(blogQuestion => <BlogCard
                         key={blogQuestion._id}
-                    >
-                        <span
-                            className='bg-gray-300 px-6 py-6 rounded-lg shadow-md'>{blogQuestion?.question}
-                            <span className='tooltip tooltip-bottom ml-5 cursor-pointer' data-tip="Show Answer">
-                                <FaArrowAltCircleRight
-                                    className='inline-block '
-                                ></FaArrowAltCircleRight>
-                            </span>
-                        </span>
-
-                    </li>)
+                        blogQuestion= {blogQuestion}
+                    ></BlogCard>)
                 }
             </div>
         </div>
--- src/Pages/Blog/blogCard.js
+++ src/Pages/Blog/blogCard.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-const blogCard = () => {
+const BlogCard = ({ blogQuestion }) => {
+    // console.log(blogQuestion)
+    // console.log(ans)
     return (
-        <div className='w-3/4 mx-auto bg-base-300 shadow-lg rounded-md'>
-            
-        </div>  
+        <div className='my-12'>
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    {blogQuestion?.question}
+                </div>
+                
+                <div className="collapse-content">
+                <div className='divider'></div>
+                    <p className='font-medium'>
+                        {blogQuestion?.ans}
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 };
 
-export default blogCard;
+export default BlogCard;
